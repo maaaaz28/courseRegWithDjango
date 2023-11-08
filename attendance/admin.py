807eb621e457246b49c325359011fb87ba2ad821
maaaaz28 <134001492+maaaaz28@users.noreply.github.com>
2023-11-08 17:10:12 +0500

    Add files via upload
    course registration with django

---
+++ attendance/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import Department, Course, Student, UserProfile, Class, Attendance
+
+# Register your models here.
+
+admin.site.register(UserProfile)
+admin.site.register(Department)
+admin.site.register(Course)
+admin.site.register(Class)
+admin.site.register(Student)
+
